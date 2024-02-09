35f6b827777579cf299eb7009d5710956c1c17a2
Bill Farmer <williamjfarmer@yahoo.co.uk>
2024-02-09 15:16:53 +0000

    Update about/help

---
+++ assets/help.md
@@ -0,0 +1,38 @@
+# BusApp
+
+Geographical GB bus stop and bus times finder.
+
+## Intro
+Scroll and zoom the map to find a bus stop and tap the map to get bus
+times from that stop.
+
+## Search
+There are several ways of finding bus times:
+
+ * **Tap the map** &ndash; This will show a list of bus times from the
+    nearest stop. You may tap a bus route to get just that route.
+ * **Tap the search button** &ndash; Type in a street name and town,
+    or postcode and tap the button on the search widget or
+    keyboard. This will show a list of bus stops or a list of
+    locations. Tap a location to get the list of bus stops.
+ * **Tap the search button** &ndash; Type in the eight character code
+    on the bus stop sign, if it exists, and tap the button on the
+    search widget or keyboard. This will show a list of bus times from
+    that stop.
+
+## Location
+The map shows a small blue circle in a blue shaded circle. The size of
+the circle indicates the accuracy of the location.The OS reference is
+shown in the left upper corner of the map. The current longitude,
+latitude, altitude are shown in the right upper corner of the map. If
+the map is panned, these figures will change to the current map
+centre.
+
+## Navigate
+You can pan and zoom the map using pinch, expand, and swipe
+gestures. The floating blue **Locate** button will return the map to
+your current location.
+
+## Permissions
+The app will ask for location permission. The location permission is
+to find out where you are.
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
 import 'package:latlong_to_osgrid/latlong_to_osgrid.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-// import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -12,6 +12,7 @@ import 'package:flutter/services.dart';
 import 'package:sprintf/sprintf.dart';
 import 'dart:async';
 
+// main
 void main() {
   runApp(
     MaterialApp(
@@ -22,14 +23,17 @@ void main() {
   );
 }
 
+// BusApp
 class BusApp extends StatefulWidget {
   const BusApp({super.key});
   @override
   _BusAppState createState() => _BusAppState();
 }
 
+// BusAppState
 class _BusAppState extends State<BusApp> {
 
+  // Formats for urls and results
   static final MULTI_FORMAT =
   'https://nextbuses.mobi/WebView/BusStopSearch/BusStopSearchResults' +
   '?id=%s&submit=Search';
@@ -41,14 +45,14 @@ class _BusAppState extends State<BusApp> {
   static final URL_FORMAT = 'https://nextbuses.mobi%s';
   static final BUS_FORMAT = '%s: %s';
 
-  static final POINT_PATTERN = RegExp(r'.+POINT\(.+\).+');
-  static final SEARCH_PATTERN = RegExp(r'.*searchMap=true.*');
+  // Pattern for bus stop codes, begins with authority code, except Scotland
   static final STOP_PATTERN = RegExp(
     r'^((nld|man|lin|bou|ahl|her|buc|shr|dvn|rtl|mer|twr|nth|cor|war|ntm|' +
     r'sta|bfs|nts|cum|sto|blp|wil|che|dor|knt|glo|woc|oxf|brk|chw|wok|' +
     r'dbs|yny|dur|soa|dby|tel|crm|sot|wsx|lan|esu|lec|suf|esx|nwm|dlo|' +
     r'lei|mlt|cej|hal|ham|sur|hrt)[a-z]{5})|[0-9]{8}$');
 
+  // State variables
   late StreamController<double?> _alignPositionStreamController;
   late AlignOnUpdate _alignPositionOnUpdate;
   late TextEditingController _controller;
@@ -59,6 +63,7 @@ class _BusAppState extends State<BusApp> {
   late bool _empty;
   late bool _busy;
 
+  // initState
   @override
   void initState() {
     super.initState();
@@ -73,12 +78,14 @@ class _BusAppState extends State<BusApp> {
     _busy = false;
   }
 
+  // dispose
   @override
   void dispose() {
     _alignPositionStreamController.close();
     super.dispose();
   }
 
+  // build
   @override
   Widget build(BuildContext context) {
     final converter = LatLongConverter();
@@ -86,6 +93,7 @@ class _BusAppState extends State<BusApp> {
       appBar: AppBar(
         title: Text('BusApp'),
         actions: [
+          // Conditional search bar
           if (_searching)
           Expanded(
             child: SearchBar(
@@ -133,12 +141,8 @@ class _BusAppState extends State<BusApp> {
                 doSearch(value);
                 _controller.clear();
               },
-              backgroundColor: MaterialStatePropertyAll(
-                ColorScheme.dark().background
-              ),
               textStyle: MaterialStatePropertyAll(
-                Theme.of(context).textTheme.bodyLarge!
-                .apply(color: ColorScheme.dark().onBackground)
+                Theme.of(context).textTheme.bodyLarge
               ),
               hintStyle: MaterialStatePropertyAll(
                 Theme.of(context).textTheme.bodyLarge!
@@ -150,6 +154,7 @@ class _BusAppState extends State<BusApp> {
           Row(
             children:
             [
+              // Search
               IconButton(
                 icon: const Icon(Icons.search),
                 onPressed: () {
@@ -158,22 +163,24 @@ class _BusAppState extends State<BusApp> {
                   );
                 }
               ),
+              // Help
               IconButton(
                 icon: const Icon(Icons.help_outline),
                 onPressed: () {
-                  Navigator.push(
-                    context,
+                  Navigator.push(context,
                     MaterialPageRoute(builder: (context) => const Help()),
                   );
                 }
               ),
+              // About
               IconButton(
                 icon: const Icon(Icons.info_outline),
-                onPressed: () {
+                onPressed: () async {
+                  PackageInfo packageInfo = await PackageInfo.fromPlatform();
                   showAboutDialog(
                     context: context,
-                    applicationName: 'BusApp',
-                    applicationVersion: 'Version 1.0',
+                    applicationName: packageInfo.appName,
+                    applicationVersion: 'Version ${packageInfo.version}',
                     applicationIcon: Image.asset('images/launch_image.png'),
                     applicationLegalese: 'Copyright © Bill Farmer' +
                     '\nLicence GPLv3',
@@ -184,12 +191,15 @@ class _BusAppState extends State<BusApp> {
           ),
         ],
       ),
+      // Map
       body: FlutterMap(
         mapController: MapController(),
         options: MapOptions(
           interactionOptions: InteractionOptions(
+            // No rotation, only works on touch screen anyway
             flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
             ),
+            // Centre of GB, possibly
           initialCenter: LatLng(52.561928, -1.464854),
           initialZoom: 6.5,
           // Buses from nearest stop
@@ -208,6 +218,7 @@ class _BusAppState extends State<BusApp> {
               _alignPositionStreamController.add(18);
               _hasChangedPosition = true;
             }
+            // Show position in top corners
             if (position.center != null) {
               var lat = position.center!.latitude;
               var lng = position.center!.longitude;
@@ -226,6 +237,7 @@ class _BusAppState extends State<BusApp> {
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'org.billthefarmer.busapp',
           ),
+          // Show position
           CurrentLocationLayer(
             alignPositionStream: _alignPositionStreamController.stream,
             alignPositionOnUpdate: _alignPositionOnUpdate,
@@ -235,8 +247,7 @@ class _BusAppState extends State<BusApp> {
             alignment: Alignment.bottomLeft,
             child: Padding(
               padding: const EdgeInsets.all(4),
-              child: Text(
-                'flutter_map',
+              child: Text('flutter_map',
                 style: Theme.of(context).textTheme.bodySmall!
                 .apply(color: Colors.black),
               ),
@@ -246,8 +257,7 @@ class _BusAppState extends State<BusApp> {
             alignment: Alignment.bottomRight,
             child: Padding(
               padding: const EdgeInsets.all(4),
-              child: Text(
-                '© OpenStreetMap contributors',
+              child: Text('© OpenStreetMap contributors',
                 style: Theme.of(context).textTheme.bodySmall!
                 .apply(color: Colors.black),
               ),
@@ -273,6 +283,7 @@ class _BusAppState extends State<BusApp> {
               ),
             ),
           ),
+          // Show busy
           if (_busy)
           Center(
             child: CircularProgressIndicator(
@@ -299,15 +310,20 @@ class _BusAppState extends State<BusApp> {
     );
   }
 
+  // busesFromPoint
   void busesFromPoint(TapPosition tapPosition, LatLng point) async {
+    // Close search bar
     if (_searching) {
       setState(() => _searching = false);
       return;
     }
+    // Set busy
     setState(() => _busy = true );
+    // Create query
     final query = sprintf(QUERY_FORMAT,
       [point.latitude, point.longitude]);
     var url = sprintf(MULTI_FORMAT, [query]);
+    // Get response and parse first stop in list
     try {
       var response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
@@ -316,11 +332,14 @@ class _BusAppState extends State<BusApp> {
         final td = table!.find('td');
         final a = td!.p!.a;
         final href = a!.getAttrValue('href');
+        // Create query
         url = sprintf(URL_FORMAT, [href]);
+        // Search buses
         busesFromUrl(url);
       }
     }
 
+    // Show error dialog
     catch (e, s) {
       showError(e);
       print(e);
@@ -328,7 +347,11 @@ class _BusAppState extends State<BusApp> {
     }
   }
 
+  // busesFromUrl
   void busesFromUrl(String url) async {
+    // Set busy
+    setState(() => _busy = true );
+    // Get response and parse buses
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
@@ -337,21 +360,29 @@ class _BusAppState extends State<BusApp> {
         final table = bs!.body!.table;
         final list = <Widget>[];
         final trs = table!.findAll('tr');
+        // Create list of buses with urls, if present
         for (final tr in trs) {
           var td = tr.find('td');
+          // Conditional bus text
           final bus = td!.p!.a?.text ?? td!.p!.nextSibling!.text;
+          // Conditional href
           final href = td!.p!.a?.getAttrValue('href');
           td = td.nextSibling;
+          // Get destination
           final dest = td!.p!.text;
+          // Add to list
           list.add(SimpleDialogOption(
               onPressed: () {
+                // Add link if href
                 if (href != null) {
                   final url = sprintf(URL_FORMAT, [href]);
                   setState(() => _busy = true );
                   busesFromUrl(url);
                 }
+                // Close dialog
                 Navigator.pop(context);
               },
+              // add dialog text
               child: Text(sprintf(BUS_FORMAT, [bus, dest]),
                 style: Theme.of(context).textTheme.bodyLarge!
                   .apply(color: ColorScheme.dark().onBackground),
@@ -360,6 +391,7 @@ class _BusAppState extends State<BusApp> {
               ),
           ));
         }
+        // Clear busy, show dialog
         setState(() => _busy = false );
         showResults(title, list);
       }
@@ -372,6 +404,9 @@ class _BusAppState extends State<BusApp> {
   }
 
   void stopsFromUrl(String url) async {
+    // Set busy
+    setState(() => _busy = true );
+    // Get response and parse stops
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
@@ -380,6 +415,7 @@ class _BusAppState extends State<BusApp> {
         final table = bs!.body!.table;
         final list = <Widget>[];
         final trs = table!.findAll('tr');
+        // Create list of stops with urls
         if (title.startsWith('Search')) {
           for (final tr in trs) {
             var td = tr.find('td');
@@ -404,6 +440,7 @@ class _BusAppState extends State<BusApp> {
           setState(() => _busy = false );
           showResults(title, list);
         }
+        // Create list of locations with urls
         else if (title.startsWith('Locations')) {
           for (final tr in trs) {
             var td = tr.find('td')!.nextSibling;
@@ -436,23 +473,29 @@ class _BusAppState extends State<BusApp> {
     }
   }
 
+  // doSearch
   void doSearch(String value) {
+    // Check value
     if (value.isEmpty)
       return;
+    // If it's a stop, get buses
     if (value.contains(STOP_PATTERN)) {
       final url = sprintf(SINGLE_FORMAT, [value]);
       busesFromUrl(url);
     }
+    // Else get stops
     else {
       final url = sprintf(MULTI_FORMAT, [value]);
       stopsFromUrl(url);
     }
   }
 
+  // showResults
   void showResults(String title, List<Widget> list) {
     showDialog<void>(
       context: context,
       builder: (BuildContext context) {
+        // Force dark theme
         return Theme(
           data: Theme.of(context).copyWith(
             dialogTheme: DialogTheme.of(context).copyWith(
@@ -473,6 +516,7 @@ class _BusAppState extends State<BusApp> {
     );
   }
 
+  // showError
   void showError(Object e) {
     showDialog<void>(
       context: context,
@@ -502,20 +546,41 @@ class _BusAppState extends State<BusApp> {
   }
 }
 
-class Help extends StatelessWidget {
+// Help
+class Help extends StatefulWidget {
   const Help({super.key});
 
+  @override
+  _HelpState createState() => _HelpState();
+}
+
+// HelpState
+class _HelpState extends State<Help> {
+
+  late String _text;
+
+  // initState
+  @override
+  void initState() {
+    super.initState();
+
+    _text = '';
+    loadAsset('assets/help.md').then((t) => setState(() => _text = t));
+  }
+
+  // build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BusApp help'),
       ),
-      body: const Markdown(data: 'Help'),
+      body: Markdown(data: _text),
     );
   }
 
-  Future<String> loadAsset() async {
-    return await rootBundle.loadString('assets/help.md');
+  // loadAsset
+  Future<String> loadAsset(String file) async {
+    return await rootBundle.loadString(file);
   }
 }
